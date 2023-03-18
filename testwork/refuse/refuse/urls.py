51c83a1e8d840eaff5d8ded07c65e9a9dbd3f02f
smoke-hux <huxleyomondu@live.com>
2023-03-18 13:17:29 +0300

    this static folder is where most of the css and changes to the css will be made

--- testwork/refuse/refuse/urls.py
+++ testwork/refuse/refuse/urls.py
@@ -13,20 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django import views
-from django.contrib import admin
-from django.urls import path
-from myapp import views
-
-
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('admin/', admin.site.urls),
-    path('home/' , views.home, name='home'),
-    path('profile/', views.profile, name='profile'),
-    path('user_info/', views.user_info, name='user_info'),
-]
 
 
 
